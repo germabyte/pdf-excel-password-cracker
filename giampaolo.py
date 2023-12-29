93d5b8bcd5b4147172356ae5a602ed26200e5360
Germano <106468361+germabyte@users.noreply.github.com>
2023-12-29 17:44:06 +0100

    Update giampaolo.py

--- giampaolo.py
+++ giampaolo.py
@@ -1,13 +1,26 @@
 import PyPDF2
 from tqdm import tqdm
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, simpledialog
 
 def select_pdf_file():
     root = tk.Tk()
     root.withdraw()  # Hide the main window
     file_path = filedialog.askopenfilename(filetypes=[("PDF files", "*.pdf")])
     return file_path
+
+def select_text_files():
+    root = tk.Tk()
+    root.withdraw()  # Hide the main window
+    file_paths = filedialog.askopenfilenames(filetypes=[("Text files", "*.txt")])
+    return file_paths
+
+def ask_for_digits():
+    root = tk.Tk()
+    root.withdraw()  # Hide the main window
+    digits = simpledialog.askinteger("Input", "Enter the number of digits for numeric brute force:",
+                                     minvalue=1, maxvalue=10)
+    return digits
 
 def try_password(reader, password):
     try:
@@ -17,21 +30,45 @@ def try_password(reader, password):
         pass
     return False
 
-def brute_force_pdf_password(pdf_path):
+def read_passwords_from_files(file_paths):
+    passwords = []
+    for file_path in file_paths:
+        with open(file_path, 'r') as file:
+            passwords += file.read().splitlines()
+    return passwords
+
+def brute_force_pdf_password(pdf_path, text_files, digits):
     with open(pdf_path, 'rb') as file:
         reader = PyPDF2.PdfReader(file)
-        for num in tqdm(range(10000), desc="Trying passwords"):
-            password = str(num).zfill(4)
+
+        # Try numeric passwords first
+        max_num = 10**digits - 1
+        for num in tqdm(range(max_num + 1), desc=f"Trying numeric passwords up to {digits} digits"):
+            password = str(num).zfill(digits)
             if try_password(reader, password):
                 print(f"\nSuccess! The password is: {password}")
                 return
+
+        # If dictionary files are provided, try passwords from them
+        if text_files:
+            dictionary_passwords = read_passwords_from_files(text_files)
+            for password in tqdm(dictionary_passwords, desc="Trying dictionary passwords"):
+                if try_password(reader, password):
+                    print(f"\nSuccess! The password is: {password}")
+                    return
+
         print("\nPassword not found.")
 
-# Get the file path from the user
+# Get the file path and number of digits from the user
 pdf_path = select_pdf_file()
-
-# Run the brute force function if a file was selected
-if pdf_path:
-    brute_force_pdf_password(pdf_path)
+digits = ask_for_digits()
+if digits is None:
+    print("No digit number selected.")
 else:
-    print("No file selected.")
+    text_files = select_text_files()
+
+    # Run the brute force function if a file was selected
+    if pdf_path:
+        brute_force_pdf_password(pdf_path, text_files, digits)
+    else:
+        print("No PDF file selected.")
